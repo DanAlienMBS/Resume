9932e057f6c1167de208a01974f6e2e8959019dc
Dan Allen <danja616@gmail.com>
2023-03-09 21:21:40 -0600

    moved files to repo

---
+++ script.js
@@ -0,0 +1,74 @@
+(function(){
+    "use strict";
+
+    const mySlides = [
+        "HTMLandCSSfreeCodeCamp.png", 
+        "CertificateHTML_CSS.png", 
+        "CertificateJavascriptBasics.png", 
+        "CertificateMetaIntroFront-end.png", 
+        "CertificateMetaJavascript.png", 
+        "HTML_CSS_inDepth.png", 
+        "ReactBasics.png"
+    ];
+    let currentImage = 0;
+    const container = document.getElementById("slideShow");
+    const nextBtn = document.getElementById("next");
+    const prevBtn = document.getElementById("previous");
+
+    nextBtn.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        currentImage++;
+        if(currentImage > (mySlides.length -1)) {
+            currentImage = 0;
+        }
+
+        const newSlide = document.createElement("img");
+        newSlide.src = `images/${mySlides[currentImage]}`;
+        newSlide.className = 'animationL';
+        newSlide.id = 'mySlide';
+        container.appendChild(newSlide);
+    
+        if(container.children.length > 2) {
+            container.removeChild(container.children[0]);
+        }
+
+        // slideSwap();
+    });
+
+    prevBtn.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        currentImage--;
+        if(currentImage < 0) {
+            currentImage = (mySlides.length - 1);
+        }
+
+        const newSlide = document.createElement("img");
+        newSlide.src = `images/${mySlides[currentImage]}`;
+        newSlide.className = 'animationR';
+         newSlide.id = 'mySlide';
+         container.appendChild(newSlide);
+    
+         if(container.children.length > 2) {
+             container.removeChild(container.children[0]);
+        }
+
+        // slideSwap();
+    });
+
+    // function slideSwap() {
+    //     const newSlide = document.createElement("img");
+    //     newSlide.src = `images/${mySlides[currentImage]}`;
+    //     newSlide.className = 'animation';
+    //     newSlide.id = 'mySlide';
+    //     container.appendChild(newSlide);
+
+    //     if(container.children.length > 2) {
+    //         container.removeChild(container.children[0]);
+    //     }
+    // }
+
+    
+
+})();
